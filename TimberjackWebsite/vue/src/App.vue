7f76a7dfa9eadb644093cdbaac34d39973ee896c
alizg005 <aliz.gosztola@gmail.com>
2024-01-08 22:30:04 -0500

    worked on navbar

--- TimberjackWebsite/vue/src/App.vue
+++ TimberjackWebsite/vue/src/App.vue
@@ -1,9 +1,32 @@
 <template>
-  <div id="capstone-app">
+
+  <!-- <div id="capstone-app">
     <div id="nav">
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">Logout</router-link>
     </div>
     <router-view />
-  </div>
+  </div> -->
+
+  <nav-bar-components></nav-bar-components>
 </template>
+
+
+<script>
+
+import NavBarComponents from './components/NavBarComponents.vue';
+
+export default {
+  components: {
+    NavBarComponents
+  }
+}
+</script>
+
+<style>
+html{
+  background-color: unset;
+}
+</style>
+
+
--- TimberjackWebsite/vue/src/views/HomeView.vue
+++ TimberjackWebsite/vue/src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
-  </div>
+<h1>Home</h1>
+
+
 </template>
 
 <script>
+
 export default {
 };
 </script>
