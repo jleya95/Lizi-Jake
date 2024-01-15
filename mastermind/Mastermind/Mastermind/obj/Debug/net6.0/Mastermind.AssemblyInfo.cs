b8c49ec8a224783e4a1b5d514c1ec3831fe478d5
unknown <jleya95@gmail.com>
2024-01-14 23:18:04 -0500

    Jake Mastermind update

--- mastermind/Mastermind/Mastermind/obj/Debug/net6.0/Mastermind.AssemblyInfo.cs
+++ mastermind/Mastermind/Mastermind/obj/Debug/net6.0/Mastermind.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mastermind")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2be0691159f5ac4ab1a6869702aefe2e27791229")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mastermind")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mastermind")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
