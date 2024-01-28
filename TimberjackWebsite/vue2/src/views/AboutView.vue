c948567585c2b5e731afb1a6c40bc33562b0bfac
unknown <jleya95@gmail.com>
2024-01-27 19:05:12 -0500

    About page Carousel mobile version (not quite working)

--- TimberjackWebsite/vue2/src/views/AboutView.vue
+++ TimberjackWebsite/vue2/src/views/AboutView.vue
@@ -35,4 +35,16 @@ export default {
     font-weight: bold;
     color: rgb(20, 91, 30);
 }
+
+@media screen and (max-width: 900px) {
+  .about{
+  margin-top: 5%;
+  background-color: rgba(255, 255, 255, 0.777);
+  margin-left: 2%;
+  margin-right: 2%;
+  border-radius: 2%;
+  padding-bottom: 2%;
+  padding-top: 2%;
+}
+}
 </style>
