40710336e37a417a6adf37eddcf66968b78707ed
unknown <jleya95@gmail.com>
2024-01-29 23:11:12 -0500

    Front end talks to back end

--- TimberjackWebsite/vue2/src/services/ContactService.js
+++ TimberjackWebsite/vue2/src/services/ContactService.js
@@ -9,10 +9,13 @@ export default {
     //         return axios.post('https://formsubmit.co/ajax/ce1e0e48a1c5a5c589191257a5a1d25c', FormData);
     //     }
     // },
+    // formSubmit(FormData) {
+    //     {
+    //         axios.defaults.headers.post['Content-Type'] = 'application/json';
+    //         return axios.post('https://formsubmit.co/ajax/57a97cd2daec8f5142e6d74ebebe82c0', FormData);
+    //     }
+    // }
     formSubmit(FormData) {
-        {
-            axios.defaults.headers.post['Content-Type'] = 'application/json';
-            return axios.post('https://formsubmit.co/ajax/57a97cd2daec8f5142e6d74ebebe82c0', FormData);
-        }
+        return axios.get(`/email?firstName=${FormData.Name.First}&lastName=${FormData.Name.Last}&addressLine1=${FormData.Address.Line1}&addressLine2=&city=${FormData.Address.City}&state=PA&zipCode=${FormData.Address.ZipCode}&country=${FormData.Address.Country}&email=${FormData.Email}&phone=${FormData.Phone}&comments=${FormData.Comments}&preference=${FormData.Preference}&service=${FormData.Services}&heardAbout=${FormData.HeardAbout}`)
     }
 };
