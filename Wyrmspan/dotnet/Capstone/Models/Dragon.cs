b56c4cf0878d087a7c5fd07b625dde0b1b62c4eb
Aliz Gosztola <aliz.gosztola@gmail.com>
2024-02-24 15:02:30 -0500

    Added end point in backend

---
+++ Wyrmspan/dotnet/Capstone/Models/Dragon.cs
@@ -0,0 +1,14 @@
+﻿namespace Wyrmspan_Backend.Models
+{
+    public class Dragon
+    {
+        public int dragon_number { get; set; }
+        public string name { get; set; }
+        public string habitat { get; set; }
+        public string size { get; set; }
+        public string cost { get; set; }
+        public string nest { get; set; }
+        public string trait { get; set; }
+        public string description { get; set; }
+    }
+}
