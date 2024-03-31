1be26cbd7536981f69dc1f45cf58f02ae95fd32b
unknown <jleya95@gmail.com>
2024-03-30 22:27:56 -0400

    TJ: switched email from Jake's to TimberJack's

--- TimberjackWebsite/vue2/src/services/ContactService.js
+++ TimberjackWebsite/vue2/src/services/ContactService.js
@@ -18,7 +18,6 @@ export default {
     formSubmit(FormData) {
         return axios.get(`https://timberjacktreeserviceapi.azurewebsites.net/email?firstName=${FormData.Name.First}&lastName=${FormData.Name.Last}&addressLine1=${FormData.Address.Line1}&addressLine2=&city=${FormData.Address.City}&state=PA&zipCode=${FormData.Address.ZipCode}&country=${FormData.Address.Country}&email=${FormData.Email}&phone=${FormData.Phone}&comments=${FormData.Comments}&preference=${FormData.Preference}&service=${FormData.Services}&heardAbout=${FormData.HeardAbout}`)
     },
-
     contactUsFormSubmit(FormData) {
         return axios.get(`https://timberjacktreeserviceapi.azurewebsites.net/contact/send?Name=${FormData.Name}&email=${FormData.Email}&phone=${FormData.Phone}&subject=${FormData.Subject}&message=${FormData.Message}`)
     }
