4ff27f68344bf264385fd41c98d84cf27bf074d5
Aliz Gosztola <aliz.gosztola@gmail.com>
2024-05-06 20:05:37 -0400

    pushing

--- LiveBurghWebsite/vue/src/components/AboutComponent.vue
+++ LiveBurghWebsite/vue/src/components/AboutComponent.vue
@@ -4,7 +4,14 @@
         <h2>About</h2>
     </div>
     <div class="description">
-        
+        <p>Liveburgh is a Music House to rehearse, record, perform,  
+            network & crash while on tour. We have intimate house shows as well as
+             performers have access to 3 Bedrooms, 2 Baths, fully equipped kitchen,
+              basement bar/gameroom, large back porch, yard, bikes, basketball, etc.
+                Within 1 mile of Hartwood Acres Park with music amphitheater as well
+                 as hiking/biking/walking trails.  
+Come check it out - You're going to have a great time!
+ </p>
     </div>
     <carousel-component></carousel-component>
 </div>
